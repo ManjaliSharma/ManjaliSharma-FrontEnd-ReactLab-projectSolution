1bccec5ebc848bd11bd261f64429da3b8ccfd46f
MANJALI SHARMA <sharmamanjali3@gmail.com>
2022-11-06 14:13:45 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ExpenseTracker} from "./components/Expense-Tracker";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <div className="App">
+    <ExpenseTracker></ExpenseTracker>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/models/expense.ts
@@ -0,0 +1,12 @@
+interface IExpenseItem {
+
+    payeeName: string,
+        expenseDescription: string,
+        price: number,
+        date: Date,
+        id: number
+}
+
+export type IExpenseCreateItem = Omit<IExpenseItem, "id">;
+
+export default IExpenseItem;
